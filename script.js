25f259ea3c0db5767c8d1c8799fbece78b0164b4
hendrik <hen.chew96@googlemail.com>
2025-08-02 20:29:04 +0200

    Add clear and delete button

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+
+let operator = document.querySelectorAll(".op-button");
+let number = document.querySelectorAll(".num-button");
+let clear = document.querySelector("#clear");
+let remove = document.querySelector("#remove");
+
+
+function eventListener() {
+    button.forEach(element => {
+        element.addEventListener("click", () => {
+            if (var1 === null) var1 = element.textContent;
+            else if (varOperator === null) varOperator = element.textContent;
+            else if (var2 === null) var2 = element.textContent;
+
+            if (var1 != null && varOperator != null && var2 != null) {
+                console.log(operate(var1, var2, varOperator));
+                var1 = null;
+                var2 = null;
+                varOperator = null;
+            }
+        })
+    })
+}
+
+function operatorEventlistener() {
+
+}
+
+function numberEventlistener() {
+
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -14,7 +46,6 @@ function divide (a, b) {
     return a / b;
 }
 
-// operator needs to be string
 function operate(a, b, operator) {
     if (operator === "+") return add(a, b);
     else if (operator === "-") return subtract(a, b);
